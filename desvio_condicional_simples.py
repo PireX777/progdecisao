7a021c47c4e45e12f2248356605587bb1e4fa22f
Raphael Martins <raphael.m.nascimento6@aluno.senai.br>
2023-08-14 11:44:13 -0300

    Initial commit

---
+++ desvio_condicional_simples.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+soma = num1 + num2
+if ( soma > 10 ):
+    print("A soma dos números inseridos é ", soma)
+
+print("FIM DO PROGRAMA")
